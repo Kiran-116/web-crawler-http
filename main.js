3983f63f29604b01ec217bd26196787a1a28dc49
Kiran <gkirankumar503@gmail.com>
2024-06-03 10:41:45 +0530

    Crawl single page with Error handling

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello World!")
+const { crawlPage } = require('./crawl.js')
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("No Website provided")
+        process.exit(1)                     // 1 -> Standard error code
+    }
+    if (process.argv.length > 3) {
+        console.log("Too many command line args")
+        process.exit(1)
+    }
+
+    const baseURL = process.argv[2]
+    console.log(`Starting Crawl of ${baseURL}`)
+
+    crawlPage(baseURL)
+}
+
+main()
